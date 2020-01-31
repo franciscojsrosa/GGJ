7d1cce0d761fbf099a602e74bcb35947f919fbcd
franciscojsrosa <franciscojsrosa@gmail.com>
2020-01-31 23:26:53 +0000

    Improved grab

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -17,31 +17,59 @@ public class MoveObject : MonoBehaviour
     {
 		rb = item.GetComponent<Rigidbody>();
 		rb.useGravity = true;
+		isHolding = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-    }
+		if (Input.GetMouseButtonDown(1))
+		{
 
-	void OnMouseDown()
+			var mainCamera = FindCamera();
+
+			// We need to actually hit an object
+			RaycastHit hit = new RaycastHit();
+			if (
+				!Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition).origin,
+								 mainCamera.ScreenPointToRay(Input.mousePosition).direction, out hit, 100,
+								 Physics.DefaultRaycastLayers))
+			{
+				return;
+			}
+			// We need to hit a rigidbody that is not kinematic
+			if (hit.rigidbody == this.GetComponent<Rigidbody>())
+			{
+				if (!isHolding)
+				{
+					rb.useGravity = false;
+					rb.isKinematic = true;
+					item.transform.position = guide.transform.position;
+					//item.transform.rotation = guide.transform.rotation;
+					item.transform.parent = tempParent.transform;
+					isHolding = true;
+					return;
+				}
+				else
+				{
+					rb.useGravity = true;
+					rb.isKinematic = false;
+					item.transform.parent = null;
+					item.transform.position = guide.transform.position;
+					isHolding = false;
+					return;
+				}
+			}
+		}
+	}
+
+	private Camera FindCamera()
 	{
-		if (!isHolding)
+		if (GetComponent<Camera>())
 		{
-			rb.useGravity = false;
-			rb.isKinematic = true;
-			item.transform.position = guide.transform.position;
-			item.transform.parent = tempParent.transform;
-			isHolding = true;
+			return GetComponent<Camera>();
 		}
-		else
-		{
-			rb.useGravity = true;
-			rb.isKinematic = false;
-			item.transform.parent = null;
-			item.transform.position = guide.transform.position;
-			isHolding = false;
-		}
+
+		return Camera.main;
 	}
 }
