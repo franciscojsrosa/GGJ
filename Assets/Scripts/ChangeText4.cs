bb26ca3d4eb69a466b2224fcd710543f438ff02f
franciscojsrosa <franciscojsrosa@gmail.com>
2020-02-01 15:36:59 +0000

    Added debug room to first level

---
+++ Assets/Scripts/ChangeText4.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeText4 : MonoBehaviour
+{
+	public GameObject player;
+	public int nextScene;
+
+	public AudioSource terminal;
+	private Text screenText;
+	private int textNum;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		screenText = this.GetComponent<Text>();
+		textNum = 1;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnMouseDown()
+	{
+		terminal.Play();
+			if (textNum == 1)
+				screenText.text = "Exception - Failed to load de_dust2\n>>What?";
+			if (textNum == 2)
+				screenText.text = "Exception - Failed to load de_dust2\n>>What?\n>>Why are you back here already?";
+			if (textNum == 3)
+				screenText.text = "Exception - Failed to load de_dust2\n>>What?\n>>Why are you back here already?\n>>Oh, nevermind, that was the wrong game.";
+			if (textNum == 4)
+				screenText.text = "Exception - Failed to load de_dust2\n>>What?\n>>Why are you back here already?\n>>Oh, nevermind, that was the wrong game.\n>>I think.";
+			if (textNum == 5)
+				screenText.text = "Exception - Failed to load de_dust2\n>>What?\n>>Why are you back here already?\n>>Oh, nevermind, that was the wrong game.\n>>I think.\n>>Let me just double check that.";
+			if (textNum == 6)
+				screenText.text = ">>What?\n>>Why are you back here already?\n>>Oh, nevermind, that was the wrong game.\n>>I think.\n>>Let me just double check that.\n>>Okay, now I'm sure, let's get back to business.";
+			if (textNum == 7)
+				screenText.text = ">>Oh, nevermind, that was the wrong game.\n>>I think.\n>>Let me just double check that.\n>>Okay, now I'm sure, let's get back to business.\n\nLoading raidshadowlegends...";
+			if (textNum == 8)
+				SceneManager.LoadScene(nextScene);
+			textNum += 1;
+	}
+}
