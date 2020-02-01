bcea99a01f77214983c02afde580d835a6852276
franciscojsrosa <franciscojsrosa@gmail.com>
2020-02-01 11:46:18 +0000

    First scene done

---
+++ Assets/Scripts/TurnLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnLight : MonoBehaviour
+{
+
+	public GameObject light;
+	public GameObject player;
+	public GameObject screen;
+
+	private Text screenText;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		screenText = screen.GetComponent<Text>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnMouseDown()
+	{
+		if ((light.transform.position - player.transform.position).sqrMagnitude < 2)
+		{
+			light.SetActive(true);
+			screenText.text = ">>Debug mode activated\n>>AI Supervisor initiated\n>>Please press Left Mouse Button to Continue...";
+		}
+	}
+}
