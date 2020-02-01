a0fdd8d49dbcd5f5d9b5c2cc22810fa6a4b6c548
Rafael Prates <fc49449@alunos.fc.ul.pt>
2020-02-01 16:09:26 +0000

    Portal: added prefab and demo

---
+++ Assets/Scripts/Portal/Sender.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sender : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject receiver;
+
+    private bool playerOverlapping = false;
+
+    void Start () {
+    }
+
+    void Update()
+    {
+        if (playerOverlapping) {
+            var currentDot = Vector3.Dot(transform.up, player.transform.position - transform.position);
+
+            if (currentDot < 0) // only transport the player once he's moved across plane
+            {
+                player.SetActive(false);
+                // transport him to the equivalent position in the other portal
+                float rotDiff = -Quaternion.Angle(transform.rotation, receiver.transform.rotation);
+                rotDiff += 180;
+                player.transform.Rotate(Vector3.up, rotDiff);               
+
+                Vector3 positionOffset = player.transform.position - transform.position;
+                positionOffset = Quaternion.Euler(0, rotDiff, 0) * positionOffset;
+                var newPosition = receiver.transform.position + positionOffset;
+                player.transform.position = newPosition;
+                
+
+                player.SetActive(true);
+                playerOverlapping = false;
+            }
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerOverlapping = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerOverlapping = false;
+        }
+    }
+}
