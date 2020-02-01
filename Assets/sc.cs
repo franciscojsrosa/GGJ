7eaf94e812d7e8167e88ee01310c446a9a412857
araag2 <artur.guimas@hotmail.com>
2020-02-01 15:46:07 +0000

    The end?

---
+++ Assets/sc.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sc : MonoBehaviour
+{
+    public float init;
+    // Start is called before the first frame update
+    void Start()
+    {
+        init = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Time.time - init > 3){
+            this.GetComponent<Text>().text = "The End?";
+        }
+    }
+}
