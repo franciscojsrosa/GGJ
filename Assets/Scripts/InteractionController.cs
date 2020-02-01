6cc1d826b6105564999dc3f747266a5b2e634c67
Rafael Prates <fc49449@alunos.fc.ul.pt>
2020-02-01 12:43:13 +0000

    Interactables: added framework for having interactable objects

---
+++ Assets/Scripts/InteractionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit info;
+            if (Physics.Raycast(transform.position, transform.forward, out info))
+            {
+                Interactable hit = info.collider.GetComponent<Interactable>();
+                if(hit != null)
+                {
+                    hit.Interact();
+                }
+            }
+        }
+       
+    }
+}
