af1a55e07291bb15c2b90d50af461c3b1551cb27
franciscojsrosa <franciscojsrosa@gmail.com>
2020-02-01 22:27:15 +0000

    Added second level

---
+++ Assets/Scripts/ChangeText5.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeText5 : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject fixer;
+	public int nextScene;
+
+	public AudioSource terminal;
+	private Text screenText;
+	private int textNum;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		screenText = this.GetComponent<Text>();
+		textNum = 1;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log(textNum);
+		terminal.Play();
+		if (textNum == 1)
+			screenText.text = ">>Oh God, what is it now?\n>>What do you mean, the enemy doesn't have a health thingy?\n";
+		if (textNum == 2)
+			screenText.text = ">>Oh God, what is it now?\n>>What do you mean, the enemy doesn't have a health thingy?\n>>Of course he's got a health thingy, that's ridiculous.";
+		if (textNum == 3)
+		{
+			screenText.text = ">>What do you mean, the enemy doesn't have a health thingy?\n>>Of course he's got a health thingy, that's ridiculous.\n>>Ugh, fine, I'll give you another one, but you're lucky I'm not charging anything.";
+			fixer.SetActive(true);
+		}
+		textNum += 1;
+	}
+}
---
+++ Assets/Scripts/TurnLight5.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnLight5 : MonoBehaviour
+{
+	public GameObject light;
+	public GameObject player;
+	public GameObject screen;
+
+	public AudioSource turningOn;
+
+	public AudioSource buzz;
+	private Text screenText;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		screenText = screen.GetComponent<Text>();
+		light.SetActive(true);
+		screenText.text = ">>Oh God, what is it now?\n";
+		buzz.Play();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
